9a0adbcdb3223ec1266406f4f1fae4276242e29b
Skrabal <skrabaldavid9@gmail.com>
2025-03-09 20:57:55 +0100

    switch

--- Program.cs
+++ Program.cs
@@ -3,146 +3,106 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace ifElse
+namespace @switch
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("zadejte den");
-            string den2 = Console.ReadLine();
-            if (den2 == "pondělí")
+            //switch= typ větvení, kde dochází k plnění podmínek
+            //      -ve své podstatě jde ostejný princip jak if , els
+            //      -vstupuje jiným způsobem do paměti
+            //       - hodnotí zadanou proměnnou a podle jejího obsahu, rozhoduje, jakým směrem se program vydá
+            //     -   VÝHODA =pro větší větvení (cca.  5 více), je to efektivnější elegatnější, než použití mnoho if příkazů
+
+            //příklad syntaxe
+            // jaký je den v týdnu podle čísla
+
+            int den = 5; //=pátek
+            switch (den)
             {
-                Console.WriteLine("začátek týdne");
+
+                case 1:
+                    Console.WriteLine("pondělí");
+                    break;
+                case 2:
+                    Console.WriteLine("úterý");
+                    break;
+                case 3:
+                    Console.WriteLine("středa");
+                    break;
+                case 4:
+                    Console.WriteLine("čtvrtek");
+                    break;
+                case 5:
+                    Console.WriteLine("pátek");
+                    break;
+                case 6:
+                    Console.WriteLine("sobota");
+                    break;
+                case 7:
+                    Console.WriteLine("neděle");
+                    break;
+
+                default:
+                    Console.WriteLine("10/10 svištů doporučuje switch");
+                    break;
             }
-            else
+            //swith příklady
+            //program, který vyhodnocuje, zda je zadané písmeno je souhlaska/ a, e ,i ,o ,u
+            char znak = 'A'; // shift ň
+            switch (znak)
             {
-                Console.WriteLine("neni začátek týdne ");
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    Console.WriteLine("samohláska");
+                    break;
+                case 'S':
+                    Console.WriteLine("svišť");
+                    break;
+                default:
+                    Console.WriteLine("souhláska");
+                    break;
             }
-            //rok
-            Console.WriteLine("zadejte zadejte rok");
-            int rok1 = Convert.ToInt32(Console.ReadLine());
-            if (rok1 % 4 == 0 && rok1 % 100 != 0 && rok1 % 400 != 0)
+            // automat na svačiny
+            //bude brát uživatelský vstup, vytvořte nabídku (např AA-šunková, AB- sýrová)
+
+
+
+            Console.WriteLine("nabídka: AA- šunková, AB- sýrová, AC- česneková AD- kuřecí");
+            string bageta = Console.ReadLine().ToUpper();
+            switch (bageta)
             {
-                Console.WriteLine("je přestupný rok");
-            }
-            else
-            {
-                Console.WriteLine("neni přestupný rok ");
-            }
-            //prošel
-            Console.WriteLine("zadejte známku");
-            int cislo4 = Convert.ToInt32(Console.ReadLine());
-            if (cislo4 > 4)
-            {
-                Console.WriteLine("neprošel");
-            }
-            else
-            {
-                Console.WriteLine("prošel");
-            }
-            //měsíc
-            Console.WriteLine("zadejte měsíc");
-            string mesic = Console.ReadLine();
-            if (mesic == "leden" || mesic == "únor" || mesic =="březen")
-            {
-                Console.WriteLine("je zima");
-            }
-            else
-            {
-                Console.WriteLine("neni zima");
+                case "AA":
+                    Console.WriteLine("vybrali jste šunkovou za 44kč");
+                    break;
+                case "AB":
+                    Console.WriteLine("vybrali jste sýrovou za 50kč");
+                    break;
+                case "AC":
+                    Console.WriteLine("vybrali jste česnekovou za 45kč");
+                    break;
+                case "AD":
+                    Console.WriteLine("vybrali jste kuřecí za 40Kč");
+                    break;
+                default:
+                    Console.WriteLine("chybná volba");
+                    break;
             }
 
-            //velikost 50
-            Console.WriteLine("zadejte číslo");
-            int cislo5 = Convert.ToInt32(Console.ReadLine());
-            if (cislo5 > 50)
-            {
-                Console.WriteLine("velké číslo");
-            }
-            else
-            {
-                Console.WriteLine("malé číslo");
-            }
-            // číslo0 až 100
-            Console.WriteLine("zadejte číslo");
-            int cislo6 = Convert.ToInt32(Console.ReadLine());
-            if (cislo6 > 0 && cislo < 100)
-            {
-                Console.WriteLine("Číslo je mezi 0 a 100.");
-            }
-            else
-            {
-                Console.WriteLine("Číslo není v požadovaném rozsahu.");
-            }
-            //admin
-            Console.WriteLine("zadejte jméno");
-            string jmeno2 = Console.ReadLine();
-            if (jmeno2 == "admin")
-            {
-                Console.WriteLine("uživatel je admin");
-            }
-            else
-            {
-                Console.WriteLine("uživatel neni admin");
-            }
-            //víkend
-            Console.WriteLine("zadejte ¨den");
-            string den = Console.ReadLine();
-            if (den == "sobota" || den == "neděle")
-            {
-                Console.WriteLine("je víkend");
-            }
-            else
-            {
-                Console.WriteLine(" není víkend");
-            }
-            //délka textu
-            Console.WriteLine("zadejte text");
-            string text = Console.ReadLine();
-            if (text.Length > 5)
-            {
-                Console.WriteLine("Dlouhý text");
-            }
-            else
-            {
-                Console.WriteLine("Krátký text");
-            }
-            //teplota
-            Console.WriteLine("zadejte teplotu");
-            int teplota = Convert.ToInt32(Console.ReadLine());
-            if (teplota > 25)
-            {
-                Console.WriteLine("Teplo");
-            }
-            else
-            {
-                Console.WriteLine("Chladno");
-            }
-            //století
-            Console.WriteLine("zadejte rok");
-            int rok = Convert.ToInt32(Console.ReadLine());
-            if (rok < 2000)
-            {
-                Console.WriteLine("20 století");
-            }
-            else
-            {
-                Console.WriteLine("¨21 století");
-            }
-            //menší než 0 nebo větší než 100
-            Console.WriteLine("zadejte teplotu");
-            int cislo7 = Convert.ToInt32(Console.ReadLine());
-            if (cislo < 0 || cislo > 100)
-            {
-                Console.WriteLine("Číslo je menší než 0 nebo větší než 100");
-            }
-            else
-            {
-                Console.WriteLine("Číslo je mezi 0 a 100");
-            }
+            //vysvětelení syntaxe-
+            //swith- nám uvozuje že bzačíná větvení
+            // (promená)- hodnota, která se kontroluje
+            //{ }- ohrádka pro dění větvení
+            //case případ, k jakému může dojít
+            // za case musí být zapsaná hodnota
+            // do case se napíše kod
+            // break _ konec větvení
+            // default- výchozí hodnota, pokud nedošlo k jinému vyhodnocen
             Console.ReadKey();
         }
     }
